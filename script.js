bab5165c2919e00ff73ba5eca5c6fb85c0506d46
Abigail Burtley <abigailtop95@yahoo.com>
2023-08-04 02:35:57 -0400

    added variables to represent the input value entered in input, time associated with the matching input and save button has an added event listener to save input value and hour of corresponding time the input was entered

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-
-
-
-var mySchedule = [
-    { Time: "9 AM", Event: ""},
-    { Time: "10 AM", Event: ""},
-    { Time: "11 AM", Event: ""},
-    { Time: "12 PM", Event: ""},
-    { Time: "1 PM", Event: ""},
-    { Time: "2 PM", Event: ""},
-    { Time: "3 PM", Event: ""},
-    { Time: "4 PM", Event: ""},
-    { Time: "5 PM", Event: ""}
-    
-    ]// array of time as key and event as value
-
-var currentDay = document.getElementById("currentDay");
-currentDay.textContent = moment(); // added time to display underneath header one 
+var saveButtns = $(".saveBtn");
+var storageInput = $('.description')
+var currentDay = $("#currentDay");
+var currentTime = $('h:mm:ss a');
 
 
 
 
+  
+
+var currentDay = document.getElementById("currentDay"); //Our instructor gave us permission to use moment js in our homework for this week
+currentDay.textContent = moment().format('MMMM Do YYYY, h:mm:ss a'); // added time to display underneath header one 
 
 
-saveBttn.addEventListener("click", saveInput);
- 
-
+$(document).ready(function() { //Acceptance Criteria: wrap jquery, so that all html elements will load first 
+  console.log("Ready!");
+  saveButtns.on('click',function() { // I want the input entered in the text area "description" to 
+var storageInput = $(this).siblings('.description'); // finds input text area with a class of description
+var inputId = storageInput.closest('.time-block').attr('id'); //this finds the correct id value closest to this specified class
+var inputText = storageInput.val(); //retrieving the value of input to save in local storage 
+localStorage.setItem(inputId,inputText); //setting key to show the hour associated with the input 
+}) 
     
 
-$(function () {
- 
- 
 })
+ 
+
+
+
+
+ 
+
+    
+
+
