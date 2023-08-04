4ee4547afc5fe3439f0a5cadadaf972685445dfa
Abigail Burtley <abigailtop95@yahoo.com>
2023-08-04 04:09:14 -0400

    added a funtion to grab the description or input value in local storage to and set the text area via desription class to save the event and make it uneditable once saved

--- script.js
+++ script.js
@@ -12,26 +12,31 @@ var currentDay = document.getElementById("currentDay"); //Our instructor gave us
 currentDay.textContent = moment().format('MMMM Do YYYY, h:mm:ss a'); // added time to display underneath header one 
 
 
+
+
 $(document).ready(function() { //Acceptance Criteria: wrap jquery, so that all html elements will load first 
   console.log("Ready!");
   saveButtns.on('click',function() { // I want the input entered in the text area "description" to 
 var storageInput = $(this).siblings('.description'); // finds input text area with a class of description
 var inputId = storageInput.closest('.time-block').attr('id'); //this finds the correct id value closest to this specified class
 var eventText = storageInput.val(); //retrieving the value of input to save in local storage 
-localStorage.setItem(inputId,eventText); //setting key to show the hour associated with the input 
+localStorage.setItem(inputId,eventText);
+storageInput.prop('readonly', true);//setting key to show the hour associated with the input 
 }) 
 });
 
 
+
+
+
 $('.description').each(function() {
   var hour = $(this).closest('.time-block').attr('id');
-  var eventSaved = localStorage.getItem("id");
+  var eventSaved = localStorage.getItem(hour);
   if (eventSaved) {
     $(this).val(eventSaved);
+    $(this).prop('readonly', true);
   }
-})
-
-
+});
  
 
 
