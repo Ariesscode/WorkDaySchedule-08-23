40e05a15cf8fa9e555425610afeec8b51085eec0
Abigail Burtley <abigailtop95@yahoo.com>
2023-08-04 10:31:39 -0400

    debug, clean code, move the document ready to the top of page because the page needs to load  to the DOM and run the code of time block classes

--- script.js
+++ script.js
@@ -4,67 +4,14 @@ var currentDay = $("#currentDay");
 var currentTime = $('h:mm:ss a');
 
 
-
-
-  
-
-var currentDay = document.getElementById("currentDay"); //Our instructor gave us permission to use moment js in our homework for this week
-currentDay.textContent = moment().format('MMMM Do YYYY, h:mm:ss A'); // added time to display underneath header one 
-
-
-
-
-//WHEN I view the timeblocks for that day
-//THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-//depends on time of color change
-//future green, greyed out past, present red
-
-
-var timeBlock = $('.time-block');
-updateTimeBlockColor();
-
-
-function updateTimeBlockColor() {
-  var currentTime = moment().hours(); //remove/add class comparing time of time block and current time 
-  $('.time-block').each(function() {
-    var hr = moment($(this).attr('id').split('-')[1], 'HH');
-    timeBlock.each(function() {
-      if (hr < currentTime) {
-        $(this).addClass('past').removeClass('present future'); //if time greater than the current hour a future color will apply and added class
-      } else if (hr === currentTime) {
-        $(this).addClass('present').removeClass('past future'); //if time less than current hour a red color will apply and added class
-      } else {
-        $(this).addClass('future').removeClass('past present'); //else which is currrent hour = to itself will show red color added to class 
-      }
-      })
-
-      
-    })
-   
-      
-      
-    setInterval(updateTimeBlockColor, 60000);
-
-}
-
-
-
-//description (this) uses for each loop to apply class
-//or remove class depended on current hour, split is used to remove hiphens,dashes or symbols 
-//between strings and numbers, parsing the class into a number , a class has quotation marks, which
-//won't equal the hour number that we need to compare 
-        
-
-
-
-
-
-
-
-
-
-$(document).ready(function() { //Acceptance Criteria: wrap jquery, so that all html elements will load first 
+$(document).ready(function() { //Acceptance Criteria: wrap jquery, so that all html elements will load first
   console.log("Ready!");
+  var currentDay = document.getElementById("currentDay"); //Our instructor gave us permission to use moment js in our homework for this week
+  currentDay.textContent = moment().format('MMMM Do YYYY, h:mm:ss A'); // added time to display underneath header one 
+  
+  updateTimeBlockColor();
+  setInterval(updateTimeBlockColor, 60000);
+
   saveButtns.on('click',function() { // I want the input entered in the text area "description" to 
 var storageInput = $(this).siblings('.description'); // finds input text area with a class of description
 var inputId = storageInput.closest('.time-block').attr('id'); //this finds the correct id value closest to this specified class
@@ -75,7 +22,27 @@ storageInput.prop('disabled', true);//setting key to show the hour associated wi
 });
 
 
+var timeBlock = $('.time-block');
 
+
+
+function updateTimeBlockColor() {
+  var currentTime = moment().startOf('hour'); //remove/add class comparing time of time block and current time 
+  timeBlock.each(function() {
+    var hr = parseInt($(this).attr('id').split('-')[1]);
+    var timeBlockTime = moment().hour(hr).startOf('hour'); 
+    
+      if (timeBlockTime.isBefore(currentTime, 'hour')) {
+        $(this).addClass('past').removeClass('present future'); //if time greater than the current hour a future color will apply and added class
+      } else if (timeBlockTime.isSame(currentTime, 'hour')) {
+        $(this).addClass('present').removeClass('past future'); //if time less than current hour a red color will apply and added class
+      } else {
+        $(this).addClass('future').removeClass('past present'); //else which is currrent hour = to itself will show red color added to class 
+      }
+      });
+setInterval(updateTimeBlockColor, 60000);
+
+}
 
 
 $('.description').each(function() { //for each function used to loop throught all text area via class of decription
